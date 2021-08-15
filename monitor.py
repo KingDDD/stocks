3d0b0fee082b2b2749e806db756fa34f65865958
Damian <dlopez2021@gmail.com>
2021-08-15 19:51:48 -0400

    refactored to separate funcionality

--- monitor.py
+++ monitor.py
@@ -7,27 +7,26 @@ from datetime import datetime
 spxl = yfinance.Ticker("SPXL")
 drn = yfinance.Ticker("DRN")
 #print(type(spxl.info))
-
-spxl_info = spxl.info
-drn_info = drn.info
 #for section,metric in stock_info.items():
 #  print(section, ":", metric)
 
 
-stock_name = spxl_info['longName']
-previous_close = spxl_info['previousClose']
-current_price = spxl_info['navPrice']
+stock_name = spxl.info['longName']
+previous_close = spxl.info['previousClose']
+current_price = spxl.info['navPrice']
 #print(stock_name)
 #print(previous_close)
 #print(current_price)
 
 #SPXL
-print(spxl_info['longName'])
+
 my_spxl = {'buyPrice' : '95.92', 'buyDate' : '04/14/2021', 'principal' : '11030.84'}
 numshares = int(float(my_spxl['principal'])/float(my_spxl['buyPrice']))
-print('Shares Held: ', numshares)
 current_position_value = numshares * float(current_price)
 diff = (current_position_value - float(my_spxl['principal']))
+
+print(spxl.info['longName'])
+print('Shares Held: ', numshares)
 print('Total $ Gain: {:.2f}'.format(diff))
 
 total_percent_gain = (diff/current_position_value) * 100
@@ -35,11 +34,11 @@ total_percent_gain = (diff/current_position_value) * 100
 print('Total % Gain: {:.2f}'.format(total_percent_gain))
 print()
 #DRN
-print(drn_info['longName'])
+print(drn.info['longName'])
 my_drn = {'buyPrice' : '18.45', 'buyDate' : '05/17/2021', 'principal' : '10152.10'}
 numshares = int(float(my_drn['principal'])/float(my_drn['buyPrice']))
 print('Shares Held: ', numshares)
-current_postion_value = numshares * float(drn_info['navPrice'])
+current_postion_value = numshares * float(drn.info['navPrice'])
 diff = (current_postion_value - float(my_drn['principal']))
 print('Total $ Gain: {:.2f}'.format(diff))
 
---
+++ refactor.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import yfinance
+
+spxl = yfinance.Ticker("SPXL")
+drn = yfinance.Ticker("DRN")
+coke = yfinance.Ticker("COKE")
+bxmt = yfinance.Ticker("BXMT")
+
+MY_SPXL = {'buyPrice' : '95.92', 'buyDate' : '04/14/2021', 'principal' : '11030.84'}
+MY_DRN = {'buyPrice' : '18.45', 'buyDate' : '05/17/2021', 'principal' : '10152.10'}
+MY_COKE = {'buyPrice' : '378.90', 'buyDate' : '05/25/2021', 'principal' : '4925.79'}
+MY_BXMT = {'buyPrice' : '31.93', 'buyDate' : '07/02/2021', 'principal' : '4789.50'}
+
+def figure(ticker, position):
+  stock = ticker
+  my_pos = position
+  
+  current_stock = {}
+  
+  stock_name = stock.info['longName']
+  previous_close = stock.info['previousClose']
+  current_price = stock.info['navPrice']
+  
+  # # of shares in position
+  numshares = int(float(my_pos['principal'])/float(my_pos['buyPrice']))
+  # total Value of position
+  if current_price is not None:
+    current_position_value = numshares * float(current_price)
+  else:
+    current_price = previous_close
+    current_position_value = numshares * float(current_price)
+  # total monetary gain
+  gain = (current_position_value - float(my_pos['principal']))
+  # total percentage gain
+  total_percent_gain = (gain/current_position_value) * 100
+  
+  # daily percentage gain
+  # daily dollar gain
+  if current_price >= previous_close:
+    daily_p_gain = (1 - previous_close/current_price) * 100
+    daily_dollar = (current_price - previous_close) * numshares
+  else:
+    daily_dollar = (previous_close - current_price) * numshares
+    daily_p_gain = (1 - current_price/previous_close) * 100  
+  
+  # build dictionary
+  current_stock["Ticker: "] = stock_name
+  current_stock["Number of Shares: "] = int(numshares)
+  current_stock["Total Position Value: $$"] = current_position_value
+  current_stock["Daily Gain: $$"] = daily_dollar
+  current_stock["Daily Percentage Change: %%"] = daily_p_gain
+  current_stock["Total Gain: $$"] = gain
+  current_stock["Total Percentage Gain: %%"] = total_percent_gain
+  
+  return current_stock
+  
+def format_output(obj):
+  output = obj
+  for key,value in output.items():
+    if isinstance(value, float):
+      print(key, "{:.2f}".format(value))
+    else:
+      print(key, value)
+      
+  print()
+  
+def main():
+  
+  working_dict = {}
+  working_dict = figure(spxl, MY_SPXL)
+  format_output(working_dict)
+  
+  working_dict = figure(drn, MY_DRN)
+  format_output(working_dict)
+  
+  working_dict = figure(coke, MY_COKE)
+  format_output(working_dict)
+  
+  working_dict = figure(bxmt, MY_BXMT)
+  format_output(working_dict)
+main()
